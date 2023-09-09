0379749433193ee35e39eaf28b0f8ae682a9cc48
AhmadMujtaba200210 <cadeta6@gmail.com>
2023-09-09 15:38:28 +0500

    demo: testing slack integration

--- backend/src/main/java/com/amigoes/fullstack/PingPongController.java
+++ backend/src/main/java/com/amigoes/fullstack/PingPongController.java
@@ -5,8 +5,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PingPongController {
-
-
     record PingPong(String result){}
     private static int COUNTER = 0;
     @GetMapping("/ping")
