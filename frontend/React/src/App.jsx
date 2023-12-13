7b00c0c53909ef333f0d604de4b14cb7ebca2995
Ahmad Mujtaba <ahmad.mujtaba@newbuffers.com>
2023-12-13 11:29:55 +0500

    test: testing environment

--- frontend/React/src/App.jsx
+++ frontend/React/src/App.jsx
@@ -30,13 +30,7 @@ const App = () => {
   if(loading){
     return(
     <SidebarWithHeader>
-      <Spinner
-        thickness='4px'
-        speed='0.65s'
-        emptyColor='gray.200'
-        color='blue.500'
-        size='xl'
-      />
+      
     </SidebarWithHeader>
     )
   }
